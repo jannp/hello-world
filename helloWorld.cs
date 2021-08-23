81f3460a1cecf213bc11c0c08b237f0e1549dc99
J Peltonen <jannp@users.noreply.github.com>
2021-08-23 20:49:24 +0300

    Update helloWorld.cs

--- helloWorld.cs
+++ helloWorld.cs
@@ -1,3 +1,10 @@
 using System;
 
 // code will be implemented here, rite?
+public class HW()
+{
+    public static void main(string[] args)
+    {
+        print("Hello my dearest world! -c#");
+    }
+}
